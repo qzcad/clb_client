9f769213abe64be1ad6ee7dcca84a70da910f131
Dmitry-Kiselev <kdem27@gmail.com>
2018-01-31 21:54:58 +0200

    change celery broker to redis

--- cluster_client/celery.py
+++ cluster_client/celery.py
@@ -7,13 +7,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cluster_client.settings')
 
 broker_host = os.environ['BROKER_HOST']
 broker_port = os.environ['BROKER_PORT']
-broker_user = os.environ['BROKER_USER']
-broker_pass = os.environ['BROKER_PASS']
+db_number = os.environ['DB_NUMBER']
 
-app = Celery('cluster_client',
-             broker='amqp://{}:{}@{}:{}'.format(broker_user, broker_pass,
-                                                broker_host, broker_port),
-             backend='rpc://')
+broker_url = 'redis://{host}:{port}/{db_number}'.format(
+    host=broker_host,
+    port=broker_port,
+    db_number=db_number
+)
+
+app = Celery('cluster_client', broker=broker_url)
+
+app.conf.result_backend = broker_url
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
