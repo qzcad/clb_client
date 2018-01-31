520131ede71a154a8693bd265dd4a0bf9d8f8f5e
Dmitry-Kiselev <kdem27@gmail.com>
2018-01-31 21:54:09 +0200

    unregister models

--- client/admin.py
+++ client/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-
-from .models import Parameter, Task, JobParameter, Job
-
-admin.site.register(Parameter)
-admin.site.register(Task)
-admin.site.register(JobParameter)
-admin.site.register(Job)
