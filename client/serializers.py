20c9eef6b9c4684ebc0cd12e44e0e1d6ca3cc3d6
Dmitry-Kiselev <kdem27@gmail.com>
2018-02-01 13:01:36 +0200

    BaseTaskSerializer refactoring

--- client/serializers.py
+++ client/serializers.py
@@ -18,17 +18,12 @@ class BaseTaskSerializer(serializers.Serializer):
     def for_func(cls, func):
         if not callable(func):
             raise NotImplementedError()
-        signature = inspect.signature(func)
+        parameters = cls._get_parameters(func)
         cl = cls._get_serializer_class_for_callable(func)
-        for field, parameter in signature.parameters.items():
+        for field, parameter in parameters.items():
             field_class = cls.serializer_field_mapping[parameter.annotation]
-            if parameter.default == inspect._empty:
-                cl._declared_fields[field] = field_class(required=True)
-            else:
-                cl._declared_fields[field] = field_class(
-                    required=False,
-                    default=parameter.default
-                )
+            field_obj = cls._get_field_for_parameter(field_class, parameter)
+            cl._declared_fields[field] = field_obj
         return cl
 
     @classmethod
@@ -43,3 +38,17 @@ class BaseTaskSerializer(serializers.Serializer):
             )
             cls.SERIALIZER_CLASSES[name] = serializer_class
         return serializer_class
+
+    @staticmethod
+    def _get_parameters(func):
+        signature = inspect.signature(func)
+        return signature.parameters
+
+    @staticmethod
+    def _get_field_for_parameter(field_class, parameter):
+        if parameter.default == inspect._empty:
+            return field_class(required=True)
+        return field_class(
+            required=False,
+            default=parameter.default
+        )
